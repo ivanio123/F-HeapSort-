eb3173b33ac7e5248f41c454989ada27490a4826
whigga_02 <jodsonjod@gmail.com>
2020-03-16 21:37:33 +0200

    Добавьте файлы проекта.

---
+++ SortTest_VS2010/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+#if VALUE_IS_DOUBLE
+	using ValueType = System.Double;
+#else
+#if VALUE_IS_SHORT
+	using ValueType = System.Int16;
+#else
+#if VALUE_IS_LONG
+	using ValueType = System.Int64;
+#else
+    using ValueType = System.Int32;
+#endif
+#endif
+#endif
+
+namespace SortTest
+{
+    class Program
+    {
+        static ValueType[] StringArrToValueArr(String[] data)
+        {
+            ValueType[] res = new ValueType[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                res[i] = ValueType.Parse(data[i]);
+            }
+            return res;
+        }
+
+        static ValueType[] InputArrFromConsole()
+        {
+            Console.WriteLine("Вводьте елементи масива в один рядок, розділяючи одинарними пропусками. Кількість елементів вводити не треба.");
+            return StringArrToValueArr(Console.ReadLine().Split());
+        }
+
+        static ValueType[] InputArrFromInputTxt()
+        {
+            StreamReader file = new StreamReader("input.txt");
+            ValueType[] res = StringArrToValueArr(file.ReadLine().Split());
+            file.Close();
+            return res;
+        }
+
+        static void PrintArr(ValueType[] arr)
+        {
+            Console.WriteLine();
+            foreach (var elem in arr)
+            {
+                Console.Write(" " + elem);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose input type: 0 - from keyboard, any other - from \"input.txt\" file");
+            String inputType = Console.ReadLine();
+            ValueType[] data;
+            if (inputType == "0")
+            {
+                data = InputArrFromConsole();
+            }
+            else
+            {
+                data = InputArrFromInputTxt();
+            }
+
+            ValueType[] arr = StudSort.Sort(data);
+            PrintArr(arr);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortTest_VS2010/StudSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if VALUE_IS_DOUBLE
+	using ValueType = System.Double;
+#else
+#if VALUE_IS_SHORT
+	using ValueType = System.Int16;
+#else
+#if VALUE_IS_LONG
+	using ValueType = System.Int64;
+#else
+using ValueType = System.Int32;
+#endif
+#endif
+#endif
+
+namespace SortTest
+{
+    class StudSort
+    {
+      
+        static void Heapify(ValueType[] arr, int heapSize, int heapRoot)
+        {
+            int max = heapRoot;
+            
+            int leftChildIndex = 2 * heapRoot + 1; 
+            int rightChildIndex = 2 * heapRoot + 2; 
+
+            
+            if (leftChildIndex < heapSize && arr[leftChildIndex] > arr[max])
+                max = leftChildIndex;
+
+            
+            if (rightChildIndex < heapSize && arr[rightChildIndex] > arr[max])
+                max = rightChildIndex;
+
+            
+            if (max != heapRoot)
+            {
+                ValueType swap = arr[heapRoot];
+                arr[heapRoot] = arr[max];
+                arr[max] = swap;
+
+                Heapify(arr, heapSize, max);
+            }
+        }
+        public static ValueType[] Sort(ValueType[] data) // DON'T CHANGE this line!!!
+        {
+            for (int i = data.Length / 2 - 1; i >= 0; i--)
+                Heapify(data, data.Length, i);
+
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                ValueType temp = data[0];
+                data[0] = data[i];
+                data[i] = temp;
+                Heapify(data, i, 0);
+            }
+            return data;
+        }
+    }
+}
